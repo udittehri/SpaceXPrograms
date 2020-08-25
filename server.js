78fbe1682302a237f7ba5ac90f48f211520e8773
Udit <udittehri@gmail.com>
2020-08-25 13:34:09 +0530

    "heroku chnages

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/sapient-assignment'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/sapient-assignment/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
